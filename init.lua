086864c01f705eb9545311a4176674b29e0116c9
Buwei <gobuwei@163.com>
2026-06-09 23:50:32 +0300

    Add lualine.nvim and replace tabby.nvim with tabline.nvim

--- init.lua
+++ init.lua
@@ -105,84 +105,30 @@ require("lazy").setup({
             end,
         }
     },
+
+    -- tabline, statusline
     {
-        'nanozuki/tabby.nvim',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            local theme = {
-                fill = 'TabLineFill',
-                head = 'TabLine',
-                current_tab = 'TabLineSel',
-                tab = 'TabLine',
-                win = 'TabLine',
-                tail = 'TabLine',
-            }
-            require('tabby').setup({
-                line = function(line)
-                    return {
-                        {
-                            { '  ', hl = { fg = '#7FBBB3', bg = '#414B50' } },
-                            line.sep('', theme.head, theme.fill),
-                        },
-                        line.tabs().foreach(function(tab)
-                            local hl = tab.is_current() and theme.current_tab or theme.tab
-
-                            -- remove count of wins in tab with [n+] included in tab.name()
-                            local name = tab.name()
-                            local index = string.find(name, "%[%d")
-                            local tab_name = index and string.sub(name, 1, index - 1) or name
-
-                            -- indicate if any of buffers in tab have unsaved changes
-                            local modified = false
-                            local win_ids = require('tabby.module.api').get_tab_wins(tab.id)
-                            for _, win_id in ipairs(win_ids) do
-                                if pcall(vim.api.nvim_win_get_buf, win_id) then
-                                    local bufid = vim.api.nvim_win_get_buf(win_id)
-                                    if vim.api.nvim_buf_get_option(bufid, "modified") then
-                                        modified = true
-                                        break
-                                    end
-                                end
-                            end
-
-                            return {
-                                line.sep('', hl, theme.fill),
-                                tab.number(),
-                                tab_name,
-                                modified and '',
-                                tab.close_btn(''),
-                                line.sep('', hl, theme.fill),
-                                hl = hl,
-                                margin = ' ',
-                            }
-                        end),
-                        line.spacer(),
-                        {
-                            line.sep('', theme.tail, theme.fill),
-                            { '  ', hl = theme.tail },
-                        },
-                        hl = theme.fill,
-                    }
-                end,
-            })
-        end,
-    },
-    {
-        "akinsho/bufferline.nvim",
-        enabled = false,
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {
-            options = {
-                numbers = "buffer_id",
-                -- Show close sign 'x' only when hovering on
-                always_show_bufferline = false,
-                hover = {
-                    enabled = true,
-                    reveal = { 'close' }
-                },
-            }
+            theme = 'gruvbox',
+            extensions = {
+                'quickfix', 'nvim-tree',
+            },
         }
     },
-    { "famiu/bufdelete.nvim",  enabled = false },
+    {
+        'kdheepak/tabline.nvim',
+        dependencies = { 'nvim-lualine/lualine.nvim' },
+        opts = {
+            options = {
+                show_filename_only = true,
+                show_devicons = false,
+                modified_italic = false,
+            },
+        }
+    },
+
     {
         'nvim-mini/mini.comment',
         opts = {
@@ -371,6 +317,10 @@ map({ "n", "v" }, "<C-j>", function() neoscroll.ctrl_d({ duration = 300 }) end)
 map({ "n", "v" }, "<C-h>", function()
     neoscroll.scroll(3, { move_cursor = false, duration = 100 })
 end)
+
+-- tabline.nvim plugin
+-- To set show_all_buffers = false in tabline.lua
+map("n", " tb", "<cmd>TablineToggleShowAllBuffers<cr>", {})
 
 ------------------------------------------------------------
 -- Token location
